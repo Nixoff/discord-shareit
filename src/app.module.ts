e0314dc704df9896a9958403886c6730f07fd27e
Sagaz <patrickmarques.job@hotmail.com>
2023-05-30 03:52:35 -0300

    fix: removing import that is bugging the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { NecordModule } from 'necord';
 import { Module } from '@nestjs/common';
 import { IntentsBitField } from 'discord.js';
-import { AppService } from './app.service';
 import { NotionModule } from './notion/notion.module';
 import { RoutineProvider } from './decorators/decorators.provider';
 
